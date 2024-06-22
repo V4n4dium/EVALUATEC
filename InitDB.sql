be21ec943d240253dbdd7d45353d6a5d5e6b5ae8
R3PL4C3DSP4CE17 <50347332+ReplacedSpace17@users.noreply.github.com>
2024-06-21 21:45:27 -0600

    Update InitDB.sql

--- InitDB.sql
+++ InitDB.sql
@@ -4,58 +4,58 @@ USE EVALUATEC;
 
 -- Create table Materias
 CREATE TABLE Materias (
-  ID int PRIMARY KEY,
-  Nombre varchar(255) NOT NULL
+  ID INT NOT NULL AUTO_INCREMENT PRIMARY KEY,  -- Auto-incrementing ID
+  Nombre VARCHAR(255) NOT NULL
 );
 
 -- Create table Docente
 CREATE TABLE Docente (
-  ID int PRIMARY KEY,
-  Nombre varchar(255) NOT NULL,
-  ApPaterno varchar(255) NOT NULL,
-  ApMaterno varchar(255) NOT NULL,
-  GradoEstudio varchar(255),
-  Foto varchar(255)
+  ID INT NOT NULL AUTO_INCREMENT PRIMARY KEY,  -- Auto-incrementing ID
+  Nombre VARCHAR(255) NOT NULL,
+  ApPaterno VARCHAR(255) NOT NULL,
+  ApMaterno VARCHAR(255) NOT NULL,
+  GradoEstudio VARCHAR(255),
+  Foto VARCHAR(255)
 );
 
 -- Create table Departamento
 CREATE TABLE Departamento (
-  ID int PRIMARY KEY,
-  Nombre varchar(255) NOT NULL
+  ID INT NOT NULL AUTO_INCREMENT PRIMARY KEY,  -- Auto-incrementing ID
+  Nombre VARCHAR(255) NOT NULL
 );
 
 -- Create table Alumno
 CREATE TABLE Alumno (
-  ID int PRIMARY KEY,
-  Email varchar(255) NOT NULL UNIQUE,
-  Password varchar(255) NOT NULL
+  ID INT NOT NULL AUTO_INCREMENT PRIMARY KEY,  -- Auto-incrementing ID
+  Email VARCHAR(255) NOT NULL UNIQUE,
+  Password VARCHAR(255) NOT NULL
 );
 
 -- Create table Puntaje
 CREATE TABLE Puntaje (
-  ID int PRIMARY KEY,
-  ID_ALUMNO int NOT NULL,
-  ID_DOCENTE int NOT NULL,
-  Calificacion int NOT NULL,
-  Opinion text,
+  ID INT NOT NULL AUTO_INCREMENT PRIMARY KEY,  -- Auto-incrementing ID
+  ID_ALUMNO INT NOT NULL,
+  ID_DOCENTE INT NOT NULL,
+  Calificacion INT NOT NULL,
+  Opinion TEXT,
   FOREIGN KEY (ID_ALUMNO) REFERENCES Alumno(ID),
   FOREIGN KEY (ID_DOCENTE) REFERENCES Docente(ID)
 );
 
 -- Create table Impartidas (Many-to-Many relationship between Docente and Materias)
 CREATE TABLE Impartidas (
-  ID_REGISTRO int PRIMARY KEY,
-  ID_DOCENTE int NOT NULL,
-  ID_MATERIA int NOT NULL,
+  ID_REGISTRO INT NOT NULL AUTO_INCREMENT PRIMARY KEY,  -- Auto-incrementing ID
+  ID_DOCENTE INT NOT NULL,
+  ID_MATERIA INT NOT NULL,
   FOREIGN KEY (ID_DOCENTE) REFERENCES Docente(ID),
   FOREIGN KEY (ID_MATERIA) REFERENCES Materias(ID)
 );
 
 -- Create table Pertenecen (Many-to-Many relationship between Docente and Departamento)
 CREATE TABLE Pertenecen (
-  ID_REGISTRO int PRIMARY KEY,
-  ID_DOCENTE int NOT NULL,
-  ID_DEPARTAMENTO int NOT NULL,
+  ID_REGISTRO INT NOT NULL AUTO_INCREMENT PRIMARY KEY,  -- Auto-incrementing ID
+  ID_DOCENTE INT NOT NULL,
+  ID_DEPARTAMENTO INT NOT NULL,
   FOREIGN KEY (ID_DOCENTE) REFERENCES Docente(ID),
   FOREIGN KEY (ID_DEPARTAMENTO) REFERENCES Departamento(ID)
 );
