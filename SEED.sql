a398bfcb8f2852e7ae100c6bf494fa9ced06c6f4
R3PL4C3DSP4CE17 <50347332+ReplacedSpace17@users.noreply.github.com>
2024-06-21 21:54:25 -0600

    Update SEED.sql

--- SEED.sql
+++ SEED.sql
@@ -1,1 +1,9 @@
+--DEPARTAMENTOS
+INSERT INTO Departamento (Nombre) VALUES ('Sistemas y computación');
+INSERT INTO Departamento (Nombre) VALUES ('Ciencias básicas');
+INSERT INTO Departamento (Nombre) VALUES ('Metal Mecánica');
+INSERT INTO Departamento (Nombre) VALUES ('Ingeniería Industrial');
+INSERT INTO Departamento (Nombre) VALUES ('Ciencias economico administrativas');
+INSERT INTO Departamento (Nombre) VALUES ('Estudios de posgrado e investigación');
 
+--MATERIAS
