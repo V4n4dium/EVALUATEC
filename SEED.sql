67c51111021c25d6f23a44bec7bb94670acc6527
V4n4dium <161883505+V4n4dium@users.noreply.github.com>
2024-06-22 19:52:10 -0600

    Update SEED.sql

--- SEED.sql
+++ SEED.sql
@@ -8,4 +8,62 @@ INSERT INTO Departamento (Nombre) VALUES ('Estudios de posgrado e investigación
 
 --MATERIAS
 INSERT INTO Materias (Nombre) VALUES ('nombre materia');
+--Primer Semestre
+INSERT INTO Materias (Nombre) VALUES ('Taller de Ética');
+INSERT INTO Materias (Nombre) VALUES ('Fundamentos de Investigación');
+INSERT INTO Materias (Nombre) VALUES ('Cálculo Diferencial');
+INSERT INTO Materias (Nombre) VALUES ('Fundamentos de Programación');
+INSERT INTO Materias (Nombre) VALUES ('Matemáticas Discretas');
+INSERT INTO Materias (Nombre) VALUES ('Taller de Administración');
+--Segundo Semestre
+INSERT INTO Materias (Nombre) VALUES ('Cálculo Integral');
+INSERT INTO Materias (Nombre) VALUES ('Álgebra Lineal');
+INSERT INTO Materias (Nombre) VALUES ('Contabilidad Financiera');
+INSERT INTO Materias (Nombre) VALUES ('Química');
+INSERT INTO Materias (Nombre) VALUES ('Programación Orientada a Objetos');
+INSERT INTO Materias (Nombre) VALUES ('Probabilidad y Estadística');
+--Tercer Semestre
+INSERT INTO Materias (Nombre) VALUES ('Desarrollo Sustentable');
+INSERT INTO Materias (Nombre) VALUES ('Cálculo Vectorial');
+INSERT INTO Materias (Nombre) VALUES ('Estructura de Datos');
+INSERT INTO Materias (Nombre) VALUES ('Cultura empresarial');
+INSERT INTO Materias (Nombre) VALUES ('Investigación de Operaciones');
+INSERT INTO Materias (Nombre) VALUES ('Física General');
+--Cuarto Semestre
+INSERT INTO Materias (Nombre) VALUES ('Ecuaciones Diferenciales');
+INSERT INTO Materias (Nombre) VALUES ('Fundamentos de Telecomunicaciones');
+INSERT INTO Materias (Nombre) VALUES ('Fundamentos de Base de Datos');
+INSERT INTO Materias (Nombre) VALUES ('Métodos Numéricos');
+INSERT INTO Materias (Nombre) VALUES ('Principios Eléctricos y Aplicaciones Digitales');
+INSERT INTO Materias (Nombre) VALUES ('Tópicos Avanzados de Programación');
+--Quinto Semestre
+INSERT INTO Materias (Nombre) VALUES ('Sistemas Operativos');
+INSERT INTO Materias (Nombre) VALUES ('Taller de Base de Datos');
+INSERT INTO Materias (Nombre) VALUES ('Fundamentos de Ingeniería de Software');
+INSERT INTO Materias (Nombre) VALUES ('Inteligencia Artificial');
+INSERT INTO Materias (Nombre) VALUES ('Arquitectura de Computadoras');
+INSERT INTO Materias (Nombre) VALUES ('Redes de Computadoras');
+--Sexto Semestre
+INSERT INTO Materias (Nombre) VALUES ('Taller de Sistemas Operativos');
+INSERT INTO Materias (Nombre) VALUES ('Administración de Base de Datos');
+INSERT INTO Materias (Nombre) VALUES ('Lenguajes de Interfaz');
+INSERT INTO Materias (Nombre) VALUES ('Conmutación y Enrutamiento en Redes de Datos');
+INSERT INTO Materias (Nombre) VALUES ('Ingeniería de Software');
+INSERT INTO Materias (Nombre) VALUES ('Lenguajes y Autómatas I');
+--Séptimo Semestre
+INSERT INTO Materias (Nombre) VALUES ('Programación Web');
+INSERT INTO Materias (Nombre) VALUES ('Graficación');
+INSERT INTO Materias (Nombre) VALUES ('Sistemas Programables');
+INSERT INTO Materias (Nombre) VALUES ('Lenguajes y Autómatas II');
+INSERT INTO Materias (Nombre) VALUES ('Gestión de Proyectos de Software');
+--Octavo Semestre
+INSERT INTO Materias (Nombre) VALUES ('Taller de Investigación I');
+INSERT INTO Materias (Nombre) VALUES ('Administración de Redes');
+INSERT INTO Materias (Nombre) VALUES ('Programación Lógica y Funcional');
+--Noveno Semestre
+INSERT INTO Materias (Nombre) VALUES ('Taller de Investigación II');
+INSERT INTO Materias (Nombre) VALUES ('Simulación');
+
+
+
 
